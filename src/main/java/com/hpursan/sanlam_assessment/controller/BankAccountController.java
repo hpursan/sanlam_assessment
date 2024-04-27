76095bed3a620205d6f626431fd28af8d0a0f4b4
hpursan <hpursan@gmail.com>
2024-04-27 20:58:51 +0200

    Added some basic logging and swagger docs. Included a bean for the SnsClient. This may need to be changed to the Asyn version

--- src/main/java/com/hpursan/sanlam_assessment/controller/BankAccountController.java
+++ src/main/java/com/hpursan/sanlam_assessment/controller/BankAccountController.java
@@ -2,34 +2,27 @@ package com.hpursan.sanlam_assessment.controller;
 
 
 import com.hpursan.sanlam_assessment.service.BankAccountService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.beans.factory.annotation.Value;
-import software.amazon.awssdk.regions.Region;
-import software.amazon.awssdk.services.sns.SnsAsyncClient;
+
 import java.math.BigDecimal;
 import java.util.concurrent.CompletableFuture;
 
-
+@Slf4j
 @RestController
+@AllArgsConstructor
 @RequestMapping("/bank")
 public class BankAccountController {
 
     private final BankAccountService bankAccountService;
-    private final SnsAsyncClient snsClient;
-
-    @Autowired
-    // the controller will have the Region value injected from the properties file
-    public BankAccountController(BankAccountService bankAccountService, @Value("${aws.region}") String region) {
-        this.bankAccountService = bankAccountService;
-        this.snsClient = SnsAsyncClient.builder().region(Region.of(region)).build();
-    }
 
     @PostMapping("/withdraw")
     public CompletableFuture<ResponseEntity<String>> withdraw( @RequestParam("accountId") Long accountId,
                                                                @RequestParam("amount") BigDecimal amount) {
+        log.info("attempt withdrawal for account id {} for an amount of {}", accountId, amount);
         return bankAccountService.withdraw(accountId, amount)
                 .thenApply(response -> ResponseEntity.status(HttpStatus.OK).body(response))
                 .exceptionally(ex -> ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage()));
